d5ddd6413fb73a7a965edf657703ff03e17d7c62
venky-ctrl <venkybandari603@gmail.com>
2025-12-15 20:04:36 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import LoginForm from "./components/LoginForm";
+import AdminDashboard from "./components/AdminDashboard";
+import UserDashboard from "./components/UserDashboard";
+import "./App.css";
 
 function App() {
+  const { user, logout } = useContext(AuthContext);
+
+  if (!user) {
+    return <LoginForm />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button className="logout-btn" onClick={logout}>
+        Logout
+      </button>
+
+      {user.role === "admin" ? <AdminDashboard /> : <UserDashboard />}
     </div>
   );
 }
